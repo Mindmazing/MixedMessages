d1cdf8e730cd5b09e05f38735f53cdc2924dea5c
Fredy <villatoro.fredy.404@gmail.com>
2024-02-26 18:52:49 -0600

    Added list of verbs, subjects and objects

---
+++ script.js
@@ -0,0 +1,3 @@
+const verbs = ["Run", "Jump", "Write", "Read", "Sing", "Dance", "Think", "Laugh", "Cook", "Paint", "Drive", "Swim", "Teach", "Learn", "Build", "Grow", "Sell", "Buy", "Throw", "Catch"];
+const subjects = ["your", "his", "her", "their", "our", "anybody's"];
+const objects = ["Chair", "Table", "Computer", "Book", "Pen", "Phone", "Cup", "Key", "Wallet", "Glasses", "Watch", "Lamp", "Car", "Guitar", "Plant", "Ball", "Shoe", "Bag"]
